f7ab801aadee15286d863a53f2a7165f29a55045
jvshoffmann <jvshoffmann@gmail.com>
2023-09-10 21:35:16 -0300

    First commit

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+
+const cors = require("cors")
+app.use(cors())
+/*app.get("/", function(req, res) {
+  res.send("It's working!")
+})*/
+app.get("/", function(req, res) {
+    res.send({"name": "Jane Doe"}) // Should be json format
+  })
+
+app.listen(3000, () => {
+  console.log("app listening on port 3000")
+})
